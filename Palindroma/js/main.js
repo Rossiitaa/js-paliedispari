fa7657346ece9d3c111a9e93366fe34e4c9d93a1
Rossiitaa <rosario.scuotto@outlook.it>
2022-06-22 16:29:28 +0200

    comments in italian

---
+++ Palindroma/js/main.js
@@ -0,0 +1,6 @@
+// creo una variabile col prompt per chiedere all'utente la parola
+//creo un'altra variabile, ma stavolta per la parola invertita
+// creo una funzione per le due parole e verifico se sono palindrome o no
+//creo una funzione per invertire la parola
+
+
