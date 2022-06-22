9c6ec29dce2de305119beb58c8a1d9838ac48399
Rossiitaa <rosario.scuotto@outlook.it>
2022-06-22 16:41:55 +0200

    palindrome finished

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -3,4 +3,25 @@
 // creo una funzione per le due parole e verifico se sono palindrome o no
 //creo una funzione per invertire la parola
 
+let normalWord = prompt('Enter here your word to find out if it is palindrome')
+let reverseWord = wordReverse(normalWord);
 
+if(normalWord === reverseWord){
+    console.log('the word is palindrome');
+} else {
+    console.log('the word is not palindrome');
+}
+
+function wordReverse(word){
+let reverseWord = '';
+
+let i = word.length - 1;
+
+while (i >= 0) {
+    reverseWord += word[i];
+
+    i--;
+}
+
+return reverseWord;
+}
